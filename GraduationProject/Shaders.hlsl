d2843b996fdeb25dd2b7e53b1f2b502f67506a55
gyowoo1113 <yereube@naver.com>
2022-06-09 01:03:34 +0900

    Test : 브레스 파티클 복구
     계속 이렇게 테스트 하면 안됨
     폭발파티클 - 브레스파티클 별개로 제작필요

--- GraduationProject/Shaders.hlsl
+++ GraduationProject/Shaders.hlsl
@@ -666,68 +666,62 @@ void GetPositions(float3 position, float2 f2Size, out float3 pf3Positions[8])
 
 
 #define PARTICLE_EXPLOSION			0x30
-#define PARTICLE_FIRST				0x31
+#define PARTICLE_LINE				0x31
 
 [maxvertexcount(9)]
 void GSParticleStreamOutput(point VS_PARTICLE_INPUT input[1], inout PointStream<VS_PARTICLE_INPUT> output)
 {
 	static int index = 0;
+	static float max_elpased = 0.0f;
 	VS_PARTICLE_INPUT particle = input[0];
 
-	if (gnParticleMode & PARTICLE_EXPLOSION) particle.age.x += gfElapsedTime;
 	if (particle.age.x <= particle.age.y)
 	{
-		//if (particle.type == PARTICLE_TYPE_EMITTER)
-		if (gnParticleMode & PARTICLE_EXPLOSION)
+		if (particle.type == PARTICLE_TYPE_EMITTER)
 		{
-			particle.color = GetParticleColor(particle.age.x, particle.age.y);
-
-			if (particle.type == PARTICLE_TYPE_EMITTER)
-			{
-				if (particle.size.x <= size_x * 20.0f)
-				{
-					float2 size = float2(size_x * 3.5f, size_y * 3.5f) * 4.0f;
-					particle.size += size * gfElapsedTime;
-				}
-			}
-			else particle.position += (0.5f * particle.acceleration * particle.velocity * gfElapsedTime);
-
-			output.Append(particle);
-		}
-		else
-		{
-			particle.color = float3(1.0f, 0.0f, 0.0f);
-			particle.size = float2(size_x, size_y);
-			//particle.type = PARTICLE_TYPE_EMITTER;
-			//			particle.age.x = 0.0f;
+			particle.color = float3(1.0f, 1.0f, 0.0f);
+			//particle.age.y = 0.25f;
+			particle.position = float3(gmtxGameObject._41, gmtxGameObject._42, gmtxGameObject._43);
 			output.Append(particle);
 
-
-			float3 pf3Positions[8];
-			GetPositions(particle.position, float2(particle.size.x * 1.25f, particle.size.x * 1.25f), pf3Positions);
-
-			particle.color = float3(0.0f, 0.0f, 1.0f);
 			particle.age.x = 0.0f;
 
 			for (int j = 0; j < 8; j++)
 			{
+				particle.color = float3(1.0f, 0.0f, 0.0f);
+
 				//float4 f4Random = gRandomBuffer.Load(uint(index * gfElapsedTime * 10000.0f) % 1000);
 				float4 f4Random = gRandomBuffer.Load(uint(index * gfCurrentTime * 100.0f) % 1000);
-				particle.type = (j == 0) ? PARTICLE_TYPE_EMITTER : PARTICLE_TYPE_FLARE;
-				particle.position = pf3Positions[j].xyz;
-				particle.velocity = float3(
-					particle.size.x * particle.age.y * 4.0f
-					, particle.size.x * particle.age.y * 4.0f
-					, particle.size.x * particle.age.y * 4.0f) * 3.0f;
 				particle.acceleration = float3(f4Random.x, f4Random.y, f4Random.z);
+				particle.type = PARTICLE_TYPE_FLARE;
+				particle.position = float3(gmtxGameObject._41, gmtxGameObject._42, gmtxGameObject._43);
+				particle.velocity = float3(3.7f, 0.0f, 0.0f) * 10000.0f;
 				particle.size = float2(size_x, size_y);
+				particle.age.y = 1.8f;
 
-				particle.age.y = 3.5f;
+				//particle.age.y = (particle.type == PARTICLE_TYPE_EMITTER) ? 0.25f : 1.5f + 0.75f * abs(j-4);
+				//particle.age.y = (particle.type == PARTICLE_TYPE_EMITTER) ? 0.25f : 1.5f + (abs(f4Random.w) * 0.75f * abs(j - 1));
+				//particle.age.y = 2.5f;
 
 				++index;
 
 				output.Append(particle);
 			}
+
+		}
+		else
+		{
+			particle.age.x += gfElapsedTime;
+			if (max_elpased <= gfElapsedTime) max_elpased = gfElapsedTime;
+
+			if (particle.size.x <= size_x * 3.0f)
+			{
+				float2 size = float2(size_x * 3.5f, size_y * 3.5f) * 1.5f;
+				particle.size += size * gfElapsedTime;
+			}
+			particle.color = GetParticleColor(particle.age.x, particle.age.y);
+			particle.position += (0.5f * particle.acceleration * particle.velocity * gfElapsedTime * gfElapsedTime);
+			output.Append(particle);
 		}
 
 	}
