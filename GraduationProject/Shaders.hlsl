1847b07780d7edf67c32ed3c1a4fcb81ed6a1002
gyowoo1113 <yereube@naver.com>
2022-06-09 13:32:44 +0900

    Test : VS에서 업데이트 되는지 테스트 (안됨)

--- GraduationProject/Shaders.hlsl
+++ GraduationProject/Shaders.hlsl
@@ -672,16 +672,15 @@ void GetPositions(float3 position, float2 f2Size, out float3 pf3Positions[8])
 [maxvertexcount(9)]
 void GSParticleStreamOutput(point VS_PARTICLE_INPUT input[1], inout PointStream<VS_PARTICLE_INPUT> output)
 {
-	static int index = 0;
 	static float max_elpased = 0.0f;
 	VS_PARTICLE_INPUT particle = input[0];
 
+	particle.age.x += gfElapsedTime;
 	if (particle.age.x <= particle.age.y)
 	{
 		if (particle.type == PARTICLE_TYPE_EMITTER)
 		{
-			particle.color = float3(1.0f, 1.0f, 0.0f);
-			//particle.age.y = 0.25f;
+			particle.color = float3(1.0f, 0.0f, 0.0f);
 			particle.position = float3(gmtxGameObject._41, gmtxGameObject._42, gmtxGameObject._43);
 			output.Append(particle);
 
@@ -692,19 +691,15 @@ void GSParticleStreamOutput(point VS_PARTICLE_INPUT input[1], inout PointStream<
 				particle.color = float3(1.0f, 0.0f, 0.0f);
 
 				//float4 f4Random = gRandomBuffer.Load(uint(index * gfElapsedTime * 10000.0f) % 1000);
-				float4 f4Random = gRandomBuffer.Load(uint(index * gfCurrentTime * 100.0f) % 1000);
+				//float4 f4Random = gRandomBuffer.Load(uint(index * gfCurrentTime * 100.0f) % 1000);
 				particle.type = PARTICLE_TYPE_FLARE;
 				particle.position = float3(gmtxGameObject._41, gmtxGameObject._42, gmtxGameObject._43);
 				particle.velocity = float3(3.7f, 0.0f, 0.0f) * 10000.0f;
 				particle.acceleration = float3(vec3.x, vec3.y, vec3.z);
+				//particle.acceleration = float3(vec3.x, vec3.y, vec3.z);
 				particle.size = float2(size_x, size_y);
 				particle.age.y = 1.8f;
-
-				//particle.age.y = (particle.type == PARTICLE_TYPE_EMITTER) ? 0.25f : 1.5f + 0.75f * abs(j-4);
-				//particle.age.y = (particle.type == PARTICLE_TYPE_EMITTER) ? 0.25f : 1.5f + (abs(f4Random.w) * 0.75f * abs(j - 1));
-				//particle.age.y = 2.5f;
-
-				++index;
+				//++index;
 
 				output.Append(particle);
 			}
@@ -712,25 +707,30 @@ void GSParticleStreamOutput(point VS_PARTICLE_INPUT input[1], inout PointStream<
 		}
 		else
 		{
-			particle.age.x += gfElapsedTime;
-			if (max_elpased <= gfElapsedTime) max_elpased = gfElapsedTime;
-
-			if (particle.size.x <= size_x * 3.0f)
-			{
-				float2 size = float2(size_x * 3.5f, size_y * 3.5f) * 1.5f;
-				particle.size += size * gfElapsedTime;
-			}
-			particle.color = GetParticleColor(particle.age.x, particle.age.y);
-			particle.position += (0.5f * particle.acceleration * particle.velocity * gfElapsedTime * gfElapsedTime);
 			output.Append(particle);
 		}
-
 	}
 }
 
+
 VS_PARTICLE_INPUT VSParticleDraw(VS_PARTICLE_INPUT input)
 {
-	return(input);
+	VS_PARTICLE_INPUT output;
+	output = input;
+	
+	output.color = input.color;
+	output.age = input.age;
+	output.type = input.type;
+	output.size = input.size;
+
+	if (output.size.x <= size_x * 4.0f)
+	{
+		float2 size = float2(size_x * 3.5f, size_y * 3.5f) * 10.5f ;
+		output.size += size * gfElapsedTime;
+	}
+	output.position += (0.5f * input.acceleration * input.velocity * gfElapsedTime * 10.0f);
+
+	return(output);
 }
 
 struct GS_PARTICLE_OUTPUT
