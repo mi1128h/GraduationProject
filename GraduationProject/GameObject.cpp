4cf9d5fff12e2ba7f6a5a7a4df37897af49261cb
gyowoo1113 <yereube@naver.com>
2022-06-29 01:45:10 +0900

    Remove : StreamParticle => RandomBuffers를 RandomValue 사용해서 세팅하도록 기존내용 제거

--- GraduationProject/GameObject.cpp
+++ GraduationProject/GameObject.cpp
@@ -5,23 +5,6 @@
 #include "Collision.h"
 #include "ParticleShader.h"
 
-inline float RandF(float fMin, float fMax)
-{
-	return(fMin + ((float)rand() / (float)RAND_MAX) * (fMax - fMin));
-}
-
-XMVECTOR RandomUnitVectorOnSphere()
-{
-	XMVECTOR xmvOne = XMVectorSet(1.0f, 1.0f, 1.0f, 1.0f);
-	XMVECTOR xmvZero = XMVectorZero();
-
-	while (true)
-	{
-		XMVECTOR v = XMVectorSet(RandF(-1.0f, 1.0f), RandF(-1.0f, 1.0f), RandF(-1.0f, 1.0f), 0.0f);
-		if (!XMVector3Greater(XMVector3LengthSq(v), xmvOne)) return(XMVector3Normalize(v));
-	}
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 CTexture::CTexture(int nTextures, UINT nTextureType, int nSamplers, int nGraphicsSrvRootParameters, int nComputeUavRootParameters, int nComputeSrvRootParameters)
@@ -1714,9 +1697,7 @@ CStreamParticleObject::CStreamParticleObject(ID3D12Device* pd3dDevice, ID3D12Gra
 	XMFLOAT4* pxmf4RandomValues = new XMFLOAT4[1000];
 	for (int i = 0; i < 1000; i++)
 	{
-		XMFLOAT3 accelations;
-		XMStoreFloat3(&accelations, ::RandomUnitVectorOnSphere());
-		pxmf4RandomValues[i] = XMFLOAT4(accelations.x, accelations.y, accelations.z, RandomValue(0.0f, 1.0f));
+		pxmf4RandomValues[i] = XMFLOAT4(RandomValue(0.0f, 1.0f), RandomValue(0.0f, 1.0f), RandomValue(0.0f, 1.0f), RandomValue(0.0f, 1.0f));
 	}
 	m_pRandowmValueTexture = new CTexture(1, RESOURCE_BUFFER, 0, 1, 0, 0);
 	m_pRandowmValueTexture->CreateBuffer(pd3dDevice, pd3dCommandList, pxmf4RandomValues, 1000, sizeof(XMFLOAT4), DXGI_FORMAT_R32G32B32A32_FLOAT, D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_GENERIC_READ, 0);
@@ -1828,9 +1809,7 @@ CStreamExplosionObject::CStreamExplosionObject(ID3D12Device* pd3dDevice, ID3D12G
 	XMFLOAT4* pxmf4RandomValues = new XMFLOAT4[1000];
 	for (int i = 0; i < 1000; i++)
 	{
-		XMFLOAT3 accelations;
-		XMStoreFloat3(&accelations, ::RandomUnitVectorOnSphere());
-		pxmf4RandomValues[i] = XMFLOAT4(accelations.x, accelations.y, accelations.z, RandomValue(0.0f, 1.0f));
+		pxmf4RandomValues[i] = XMFLOAT4(RandomValue(0.0f,1.0f), RandomValue(0.0f, 1.0f), RandomValue(0.0f, 1.0f), RandomValue(0.0f, 1.0f));
 	}
 	m_pRandowmValueTexture = new CTexture(1, RESOURCE_BUFFER, 0, 1, 0, 0);
 	m_pRandowmValueTexture->CreateBuffer(pd3dDevice, pd3dCommandList, pxmf4RandomValues, 1000, sizeof(XMFLOAT4), DXGI_FORMAT_R32G32B32A32_FLOAT, D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_GENERIC_READ, 0);
